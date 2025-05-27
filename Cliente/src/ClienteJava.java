9ba52b7e46fff88abec115e6b545e606bb0a5177
unknown <yerikchaves818@gmail.com>
2025-05-27 01:14:41 -0600

    Conexion inicial entre Java y C

---
+++ Cliente/src/ClienteJava.java
@@ -0,0 +1,31 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// ClienteJava.java
+import java.io.*;
+import java.net.*;
+
+public class ClienteJava {
+    public static void main(String[] args) {
+        String servidor = "localhost";
+        int puerto = 12345;
+
+        try (Socket socket = new Socket(servidor, puerto);
+             OutputStream salida = socket.getOutputStream();
+             InputStream entrada = socket.getInputStream()) {
+
+            // Enviar mensaje al servidor
+            String mensaje = "Hola desde el cliente Java!";
+            salida.write(mensaje.getBytes());
+            salida.flush();
+
+            // Leer respuesta
+            byte[] buffer = new byte[1024];
+            int leido = entrada.read(buffer);
+            String respuesta = new String(buffer, 0, leido);
+            System.out.println("Respuesta del servidor: " + respuesta);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
