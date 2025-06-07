ce4eee63656da949960df59cafd3034ef67baeea
gabonm14 <88728736+gabonm14@users.noreply.github.com>
2025-06-07 17:02:48 -0600

    bencaos, implementacao pantalla do jogo

--- Cliente/controller/MainViewController.java
+++ Cliente/controller/MainViewController.java
@@ -3,12 +3,37 @@ package controller;
 import cliente.ClienteJava;
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+import javafx.stage.Window;
 
 public class MainViewController {
     // Aquí van los métodos y @FXML campos conectados al FXML
     @FXML
     private void manejarBoton(ActionEvent event) {
         ClienteJava.iniciarClienteDesdeGUI(); // Aquí se llama al cliente
+
+        try {
+            // Cargar la nueva ventana
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/GameView.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Ventana de Juego");
+            stage.show();
+
+            // Cerrar la ventana actual
+            Window ventanaActual = ((Node) event.getSource()).getScene().getWindow();
+            ventanaActual.hide();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
 
